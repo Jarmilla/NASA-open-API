a3681d463b19e8544ecf96f1a7ff3a12b20cb4d6
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-05-21 15:40:34 +0200

    fetch working

--- nasa-api/src/App.js
+++ nasa-api/src/App.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+const key = "eScw6dlpwJ2AIBuldX1hVZD5Bl1TcnLm9GfqgRwx";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
+  .then((res) => res.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log("Error: ", err));
+
 function App() {
   return (
     <div className="App">
