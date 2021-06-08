ac5d22c7ace9d66c91e0fa7d9ea0b6798b162dec
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-06-08 17:43:00 +0200

    inputDate cannot be less than the date of the first image

--- nasa-api/src/App.js
+++ nasa-api/src/App.js
@@ -9,18 +9,23 @@ function App() {
   const [choosenApodData, setChoosenApodData] = useState({});
   const [apodDatas, setApodDatas] = useState(null);
 
-  function startDate(inputDate) {
-    let transformToDate = new Date(inputDate);
+  function endDateValidation(inputDate) {
+    return inputDate < "1995-06-22" ? "1995-06-22" : inputDate;
+  }
+
+  function startDateGeneration(date) {
+    let transformToDate = new Date(date);
     transformToDate.setDate(transformToDate.getDate() - 6);
     return transformToDate.toISOString().slice(0, 10);
   }
 
   useEffect(() => {
-    let weekBeforeInputDate = startDate(inputDate);
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=${weekBeforeInputDate}&end_date=${inputDate}`)
+    let endDate = endDateValidation(inputDate);
+    let startDate = startDateGeneration(endDate);
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}&start_date=${startDate}&end_date=${endDate}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(inputDate, weekBeforeInputDate, data);
+        console.log(endDate, startDate, data);
         setChoosenApodData(data[data.length - 1]);
         setApodDatas(data);
       })
@@ -31,7 +36,8 @@ function App() {
     <div>
       <div className="main-container">
         <h1>Astronomy Picture of the Day</h1>
-        <input type="date" onChange={(e) => setInputDate(e.target.value)} />
+        <input type="date" onInput={(e) => setInputDate(e.target.value)} />
+        {inputDate < "1995-06-22" ? <p>Must be after 1995-06-21</p> : ""}
 
         <AstronomyPictureOfTheDay choosenApodData={choosenApodData} />
       </div>
